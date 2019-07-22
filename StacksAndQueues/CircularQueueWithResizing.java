daa7d34faa1401f3a2b812b7a5e302422a60578d
Nishant Rawat <nishantrawat22@gmail.com>
2019-07-21 18:48:18 -0700

    Update Circular queue with resizing

--- StacksAndQueues/CircularQueueWithResizing.java
+++ StacksAndQueues/CircularQueueWithResizing.java
@@ -8,15 +8,20 @@ public static class Queue{
 	}
 
 	public void enqueue(Integer n){
+		///Increase the size of the array when array is full
 		if(numQueueElements == entries.length){ //array is full
-			Collections.rotate(Arrays.asList(entries), -head);
+			Collections.rotate(Arrays.asList(entries), -head); // we are rotating
+			//by head, in case the easier cells of the array are empty.
 			head = 0;
 			tail = numQueueElements;
-			entries = Arrays.copyOf(entries, numQueueElements + SCALE_FACTOR);
+			entries = Arrays.copyOf(entries, numQueueElements * SCALE_FACTOR); // increase
+			//the size of the arrays.
 		}
 
 		entries[tail] = n;
-		tail = (tail + 1) % entries.length;
+		tail = (tail + 1) % entries.length; //if tail was 10, then now tail is (10+1)%(20),
+		//because entries.length is 20 since it got doubled, so that modulus
+		//will only return 11%20 i.e. 11
 		++numQueueElements;
 	}
 
@@ -24,7 +29,10 @@ public static class Queue{
 		if(numQueueElements != 0){
 			--numQueueElements;
 			Integer ret = entries[head];
-			head = (head+1)%entries.length;
+			head = (head+1)%entries.length; //We are just moving the head forward, whenever an element is dequeued
+			//If head is 1, then head becomes 2 on dequeue...
+			//WHat do we do for all the empty spaces....
+			//Well, we are ROTATING, that pushes the empty spaces back.
 			return ret;
 		}
 		System.out.println("Queue is empty");
