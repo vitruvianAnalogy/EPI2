c9624a73a10394d3ed78170fddcc890579988603
Nishant Rawat <nishantrawat22@gmail.com>
2019-12-14 15:54:35 -0700

    Added simple tidbits about the coding pattern

--- IntelliJ/src/com/educative/slidingwindow/LongestSubStringWithKDistinctCharacters.java
+++ IntelliJ/src/com/educative/slidingwindow/LongestSubStringWithKDistinctCharacters.java
@@ -1,4 +1,8 @@
 package com.educative.slidingwindow;
 
 public class LongestSubStringWithKDistinctCharacters {
+    public static int findLength(String str, int k){
+        int length = str.length();
+
+    }
 }
