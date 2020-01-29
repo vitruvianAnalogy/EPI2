62bcc10c362b99315347ce641f9bc33365be3ed0
Nishant Rawat <nishantrawat22@gmail.com>
2020-01-28 23:33:41 -0700

    conflicting appointments problem - merge intervals pattern

--- IntelliJ/src/com/educative/mergeintervals/ConflictingAppointments.java
+++ IntelliJ/src/com/educative/mergeintervals/ConflictingAppointments.java
@@ -1,4 +1,23 @@
 package com.educative.mergeintervals;
 
+import java.util.*;
+
 public class ConflictingAppointments {
+    public static boolean canAttendAllAppointments(Interval[] intervals) {
+        // sort the intervals by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+
+        // find any overlapping appointment
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                // please note the comparison above, it is "<" and not "<="
+                // while merging we needed "<=" comparison, as we will be merging the two
+                // intervals having condition "intervals[i].start == intervals[i - 1].end" but
+                // such intervals don't represent conflicting appointments as one starts right
+                // after the other
+                return false;
+            }
+        }
+        return true;
+    }
 }
